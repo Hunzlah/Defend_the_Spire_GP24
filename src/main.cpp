a91a372fdfbc01f4335e9350d2adc57d0bebf037
Hunzlah <56739241+Hunzlah@users.noreply.github.com>
2025-03-22 04:45:54 +0100

    Enemy spawn method

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,35 @@
 #include "Constants.h"
 #include "Enemy.h"
 
+Grid grid;
+vector<Enemy> enemies;
+float enemySpawnTimer;
 
+void SetEnemyNextTargetPosition(Enemy* enemy)
+{
+    Cell enemyTargetCell = grid.GetNextPoint(enemy->cellPostion.x, enemy->cellPostion.y);
+    enemy->SetCellPosition((Vector2){enemyTargetCell.positionX, enemyTargetCell.positionY});
+    Vector2 targetPos = {enemyTargetCell.positionX * cellSize + cellSize / 2, enemyTargetCell.positionY*cellSize + cellSize / 2};
+    enemy->SetTarget(targetPos);
+}
+void SpawnNewEnemy()
+{
+    // Enemy starts at top-left corner
+    Vector2 enemyPos = { 0.0f, 0.0f };
+    
+    Enemy enemy(enemyPos);
+    enemy.SetCellPosition(enemyPos);
 
+    
+    SetEnemyNextTargetPosition(&enemy);
 
+    enemies.push_back(enemy);
+}
 int main() {
     InitWindow(screenWidth, screenHeight, "Grid with Tower and Enemy");
     SetTargetFPS(60);
 
-    Grid grid;
+    
 
     // Tower at center
     int towerX = cols / 2;
@@ -19,31 +40,36 @@ int main() {
     grid.SetTowerCell(towerX, towerY, TOWER);
     grid.SetPathToTower(towerX, towerY);
 
-    // Enemy starts at top-left corner
-    Vector2 enemyPos = { 0.0f, 0.0f };
-    Vector2 towerPos = { towerX * cellSize + cellSize / 2, towerY * cellSize + cellSize / 2 };
-    Enemy enemy(enemyPos);
-    enemy.SetCellPosition(enemyPos);
-    Cell enemyTargetCell = grid.GetNextPoint(enemy.cellPostion.x, enemy.cellPostion.y);
-    enemy.SetCellPosition((Vector2){enemyTargetCell.positionX, enemyTargetCell.positionY});
-    Vector2 targetPos = {enemyTargetCell.positionX * cellSize + cellSize / 2, enemyTargetCell.positionY*cellSize + cellSize / 2};
-    enemy.SetTarget(targetPos);
+    //SpawnNewEnemy();
 
-    while (!WindowShouldClose()) {
-        enemy.Update();
-        if(enemy.CurrentDistanceToTarget() <= 1)
-        {
-            enemyTargetCell = grid.GetNextPoint(enemy.cellPostion.x, enemy.cellPostion.y);
-            enemy.SetCellPosition((Vector2){enemyTargetCell.positionX, enemyTargetCell.positionY});
-            Vector2 targetPos = {enemyTargetCell.positionX * cellSize + cellSize / 2, enemyTargetCell.positionY*cellSize + cellSize / 2};
-            enemy.SetTarget(targetPos);
-        }
-
+    while (!WindowShouldClose()) 
+    {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        enemySpawnTimer += GetFrameTime();
+
+        if(enemySpawnTimer > 2)
+        {
+            SpawnNewEnemy();
+            enemySpawnTimer = 0;
+        }
+
+        for(Enemy& enemy : enemies )
+        {
+            enemy.Update();
+            if(enemy.CurrentDistanceToTarget() <= 1)
+            {
+                SetEnemyNextTargetPosition(&enemy);
+            }
+            enemy.Draw();
+        }
+        
+
+        
+
         grid.Draw();
-        enemy.Draw();
+        
 
         EndDrawing();
     }
